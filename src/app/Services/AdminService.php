8f26ada045fd334f7976f7c1ccc54b36e95f35df
Mohamed Khedr <raid@localhost.localdomain>
2024-04-12 22:55:07 +0200

    Add account service

--- src/app/Services/AdminService.php
+++ src/app/Services/AdminService.php
@@ -2,31 +2,14 @@
 
 namespace App\Services;
 
+use App\Core\Services\AccountService;
 use App\Core\Services\Contracts\ServiceInterface;
-use App\Core\Services\Service;
 use App\Repositories\Contracts\AdminRepositoryInterface;
-use Illuminate\Auth\AuthenticationException;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Config;
 
-class AdminService extends Service implements ServiceInterface
+class AdminService extends AccountService implements ServiceInterface
 {
     public function __construct(AdminRepositoryInterface $repository)
     {
         $this->setRepository($repository);
-    }
-
-    /**
-     * @throws AuthenticationException
-     */
-    public function login(array $data): string
-    {
-        $token = auth()->attempt($data);
-
-        if (! $token) {
-            throw new AuthenticationException(__('auth.failed'));
-        }
-
-        return $token;
     }
 }
--- src/app/Services/UserService.php
+++ src/app/Services/UserService.php
@@ -2,50 +2,17 @@
 
 namespace App\Services;
 
+use App\Core\Services\AccountService;
 use App\Core\Services\Contracts\ServiceInterface;
 use App\Core\Services\Service;
 use App\Repositories\Contracts\UserRepositoryInterface;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Contracts\Auth\Authenticatable;
 
-class UserService extends Service implements ServiceInterface
+class UserService extends AccountService implements ServiceInterface
 {
     public function __construct(UserRepositoryInterface $repository)
     {
         $this->setRepository($repository);
-    }
-
-    /**
-     * @throws AuthenticationException
-     */
-    public function login(array $data): string
-    {
-        $token = auth()->attempt($data);
-
-        if (! $token) {
-            throw new AuthenticationException(__('auth.failed'));
-        }
-
-        return $token;
-    }
-
-    public function getProfile(): Authenticatable
-    {
-        return auth()->user();
-    }
-
-    public function updateProfile(array $data): bool
-    {
-        return $this->update(auth()->user(), $data);
-    }
-
-    public function updatePassword(array $data): bool
-    {
-        return $this->update(auth()->user(), $data);
-    }
-
-    public function logout(): void
-    {
-        auth()->logout(true);
     }
 }
