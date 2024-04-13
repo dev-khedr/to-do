740545c0a57671394165da328599c2325ae4cfcf
Mohamed Khedr <raid@localhost.localdomain>
2024-04-13 11:03:08 +0200

    Update readme file

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -39,9 +39,4 @@ class User extends Authenticatable implements JWTSubject
     {
         return $this->hasMany(TaskList::class);
     }
-
-    public function tasks(): HasMany
-    {
-        return $this->taskLists->tasks();
-    }
 }
