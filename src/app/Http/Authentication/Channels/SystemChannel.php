3578b5f0210deadc6605900ac320d334773a2c96
Mohamed Khedr <raid@localhost.localdomain>
2024-04-19 13:57:19 +0200

    Implement email and phone steps

--- src/app/Http/Authentication/Channels/SystemChannel.php
+++ src/app/Http/Authentication/Channels/SystemChannel.php
@@ -4,10 +4,10 @@ namespace App\Http\Authentication\Channels;
 
 use Raid\Core\Authentication\Channels\Channel;
 use Raid\Core\Authentication\Channels\Contracts\ChannelInterface;
-use Raid\Core\Authentication\Channels\Contracts\Concerns\RunRules;
+use Raid\Core\Authentication\Channels\Contracts\Concerns\ShouldRunRules;
 use Raid\Core\Authentication\Traits\Channels\HasRules;
 
-class SystemChannel extends Channel implements ChannelInterface, RunRules
+class SystemChannel extends Channel implements ChannelInterface, ShouldRunRules
 {
     use HasRules;
 
--- src/app/Http/Authentication/Channels/TwoFactorEmailChannel.php
+++ src/app/Http/Authentication/Channels/TwoFactorEmailChannel.php
@@ -4,12 +4,12 @@ namespace App\Http\Authentication\Channels;
 
 use Raid\Core\Authentication\Channels\Channel;
 use Raid\Core\Authentication\Channels\Contracts\ChannelInterface;
-use Raid\Core\Authentication\Channels\Contracts\Concerns\RunRules;
-use Raid\Core\Authentication\Channels\Contracts\Concerns\RunSteps;
+use Raid\Core\Authentication\Channels\Contracts\Concerns\ShouldRunRules;
+use Raid\Core\Authentication\Channels\Contracts\Concerns\ShouldRunSteps;
 use Raid\Core\Authentication\Traits\Channels\HasRules;
 use Raid\Core\Authentication\Traits\Channels\HasSteps;
 
-class TwoFactorEmailChannel extends Channel implements ChannelInterface, RunRules, RunSteps
+class TwoFactorEmailChannel extends Channel implements ChannelInterface, ShouldRunRules, ShouldRunSteps
 {
     use HasRules;
     use HasSteps;
