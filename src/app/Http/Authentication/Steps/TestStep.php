eebb6189b7c1dbb1644829ce5dd7abe5fda15e74
Mohamed Khedr <raid@localhost.localdomain>
2024-04-26 16:28:53 +0300

    Test commands

--- src/app/Http/Authentication/Steps/TestStep.php
+++ src/app/Http/Authentication/Steps/TestStep.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Authentication\Steps;
 
+use Raid\Core\Authentication\Channels\Contracts\ChannelInterface;
 use Raid\Core\Authentication\Steps\Contracts\StepInterface;
 
 class TestStep implements StepInterface
--- src/app/Http/Authentication/Workers/TestWorker.php
+++ src/app/Http/Authentication/Workers/TestWorker.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Authentication\Workers;
 
+use Raid\Core\Authentication\Workers\Worker;
+use Raid\Core\Authentication\Workers\Contracts\WorkerInterface;
+
 class TestWorker extends Worker implements WorkerInterface
 {
     public const ATTRIBUTE = '';
